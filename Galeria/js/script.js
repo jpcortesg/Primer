134f3ca64fcf741b44474ecc4af582a0cef84780
jpcortesg <juan2001cortes.jc@gmail.com>
2020-04-12 19:08:36 -0500

    Subir ultimos proyectos y cambios realizados

---
+++ Galeria/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+    $('#magnify2').click(function(){
+      $(this).hide();
+      $('#search').toggle('scale');
+      $('#search').css('display','block');
+    });
+
+});
+
